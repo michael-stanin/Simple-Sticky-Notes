c70eae14b48dcdac39ec6c951d97419277523b71
michael-stanin <michael.stanin@gmail.com>
2015-02-04 17:15:25 +0200

    Initial login/register handles

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+"use strict"
 
+$(document).ready(function () {
+	$('.login').on('click', function() {
+		if (validate()) {
+			doesUserExist(username, password);
+		}
+	});
+
+	$('.register').on('click', function() {
+		if(validate()) {
+			addNewUser();
+		}
+	});
+
+	function addNewUser() {
+		// Add the user to the local storage.	
+	}
+
+	function doesUserExist(username, password) {
+		// Check if the user is in the local storage
+	}
+
+	function loadUserStickyNotes () {
+		// Load the user's notes
+	}
+
+	function validate() {
+		var username = $('#username').val();
+		var password = $('#username').val();
+
+		if (username === "") {
+			alert("Enter valid username");
+			return false;
+		}
+		if (password === "") {
+			alert("Enter valid password");
+			return false;
+		}
+
+		return true;
+	}
+});
